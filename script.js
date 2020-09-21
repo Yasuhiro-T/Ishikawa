345fc17c39c5a9ce4f3efdf2b12bea90209eb24a
Yasuhiro-T <71485481+Yasuhiro-T@users.noreply.github.com>
2020-09-21 20:42:52 +0900

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ $(function(){
         var scroll_top = $(window).scrollTop();
         $('.start').each(function(){
             var elem_pos = $(this).offset().top;
-            if(window_h + scroll_top > elem_pos + 300){
+            if(window_h + scroll_top > elem_pos + 200){
                 $(this).addClass('fadein');
             }else{
                 $(this).removeClass('fadein');
